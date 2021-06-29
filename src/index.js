1ca6fff19baecf47acd0c224066509d46dcd603f
Elias Moura <d.eliasmoura2@gmail.com>
2021-06-29 12:27:13 -0300

    config running without .env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const main = async ()=>{
   await app.listen(PORT, () => {
